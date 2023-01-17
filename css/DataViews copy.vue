baa8d11ced1592157582f95a86cf2a0cf882164e
CloudPureNO1 <CloudPure@163.com>
2023-01-17 17:02:13 +0800

    bigscreen html

---
+++ css/DataViews copy.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="main-data-views">
+        <div class="header">
+            <h1>残疾人安置数据</h1>
+        </div>
+        <div class="main-box">
+
+            <div class="column">
+                <div class="panel">
+                    <div class="title">
+                        <h2>残疾人分布折线图</h2>
+                    </div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+
+                <div class="panel">
+                    <div class="title"></div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+                <div class="panel">
+                    <div class="title"></div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="panel-num">
+                    <div class="panel-num-header">
+                        <ul>
+                            <li>98777452</li>
+                            <li>123455</li>
+                        </ul>
+                    </div>
+                    <div class="panel-num-body">
+                        <ul>
+                            <li>申报人数</li>
+                            <li>核定人数</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="panel-map">
+                    <div class="map-bg1"></div>
+                    <div class="map-bg2"></div>
+                    <div class="map-bg3"></div>
+                    <div class="conten"></div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="panel">
+                    <div class="title"></div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+                <div class="panel">
+                    <div class="title"></div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+                <div class="panel">
+                    <div class="title"></div>
+                    <div class="content"></div>
+                    <div class="panel-footer"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import '../assets/css/dataView.css'; /* 这个分号一定要写，要不会报错 */
+</style>
